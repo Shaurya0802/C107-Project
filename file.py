3971766d39c708450d2f81b5f582252f7b267760
Shaurya0802 <shauryaakhileshsharma@gmail.com>
2021-01-18 11:24:51 +0530

    C107-Project

--- file.py
+++ file.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv("data.csv")
-print(df)
-fig = px.scatter(df.groupby("level")["attempt"].mean(), x="student_id", y=["Level 1", "Level 2", "Level 3", "Level 4"], color="attempt", size="attempt")
+mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
+
+fig = px.scatter(mean, x="student_id", y="level", color="attempt", size="attempt")
 fig.show()
